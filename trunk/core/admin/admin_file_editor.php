8a0731842f76d3f2bb58256934797b94ec235ad3
mathieu.vilaplana <mathieu.vilaplana@37888572-14cc-11de-82b5-194b17410922>
2009-04-07 14:11:08 +0000

    modification du admin file editor. Ajout de la fonction display editor à l'objet PPage

--- trunk/core/admin/admin_file_editor.php
+++ trunk/core/admin/admin_file_editor.php
@@ -42,11 +42,11 @@ if($pfile === false){
 $pdirparent = &getFileObject($pfile->getParentPath());
 
 if( isConnected() ){
-	
-?>
-<div id="path">
-<div id="imgHome"></div>
-<?php
+	echo '
+		<div id="path">
+		<div id="imgHome"></div>
+	';
+
 	$strPath = $pfile->path;
 	$oRoot = new PDir( (strstr($pfile->path,PAGES_PATH)!==false)?PAGES_PATH:((strpos($pfile->path,PAGES_MODELS_PATH)!==false)?PAGES_MODELS_PATH:SITE_PATH));
 	
@@ -65,99 +65,11 @@ if( isConnected() ){
 		echo '<a href="'.$strUrlGuid['URL'].'">'.$strUrlGuid['NAME'].'</a> > ';
 	}
 	echo $pfile->getShortName();
-?>
-</div>
-<?php
-if(eregi(TEXTEDIT_WYSWYG,$pfile->getname())){
-	$strFCKConfigDir = SITE.SLASH.'config'.SLASH.'fckeditor'.SLASH;
-	$strFCKConfigUrl = SITE_URL.((SLASH=='/')?$strFCKConfigDir:str_replace(SLASH,'/',$strFCKConfigDir)); 
-	$strFCKConfigDir = CONFIG_DIR.'fckeditor'.SLASH;
-	
-	$oFCKeditor = new FCKeditor('text') ;
-	$oFCKeditor->BasePath = SITE_URL.'vendors/jscripts/fckeditor/';
-	$oFCKeditor->Value = $pfile->getEditorFileContent();
-	$oFCKeditor->Height = '400' ;
-	
-	//the styles to apply to the editor, the page style and the fckeditor.css style
-	$fileStyleUrls='';
-	if($pageStyle = $pfile->getConfig("PAGE_STYLE")) $fileStyleUrls .= THEME_URL.'css/'.$pageStyle.',';
-	if(is_file($strFCKConfigDir.'fckeditor.css')) $fileStyleUrls .= $strFCKConfigUrl.'fckeditor.css';
-	$oFCKeditor->Config["EditorAreaCSS"] =$fileStyleUrls;
-	
-	$oFCKeditor->Config["ImageBrowserURL"] = SITE_URL."core/admin/admin_file_selector.php?rootpath=".urlencode(POFile::getPathRelativePath(MEDIAS_PATH));
-	$oFCKeditor->Config["FlashBrowserURL"] = SITE_URL."core/admin/admin_file_selector.php?rootpath=".urlencode(POFile::getPathRelativePath(MEDIAS_PATH));
-	//go to the parent path
-	$oFCKeditor->Config["LinkBrowserURL"] = SITE_URL."core/admin/admin_file_selector.php?current_dir=".urlencode(POFile::getPathRelativePath($pfile->getParentPath(), PAGES_PATH))."&rootpath=".urlencode(POFile::getPathRelativePath(PAGES_PATH));
-	$oFCKeditor->Config["CustomConfigurationsPath"] = SITE_URL.'?page=fckconfig.js';
-	
-	if(is_file($strFCKConfigDir.'fcktemplates.xml'))
-		$oFCKeditor->Config["TemplatesXmlPath"]	= SITE_URL.'?page=fcktemplates.xml' ;
-	if(is_file($strFCKConfigDir.'fckstyles.xml'))
-		$oFCKeditor->Config["StylesXmlPath"]	= SITE_URL.'?page=fckstyles.xml' ;
-	
-	
-	$strBarName = getUserEditorBar();
-	if($strBarName) $oFCKeditor->ToolbarSet = $strBarName;
-	
 
-	$strTabsTpl ='
-	<div id="tabPageEditor">
-		<ul>
-			<li class="ui-tabs-nav-item"><a href="#fragEditor"><span>Edition</span></a></li>
-			<li class="ui-tabs-nav-item"><a href="#fragOptions"><span>Options</span></a></li>
-		</ul>
-		
-		<div id="fragEditor">{TAB_EDITOR}</div>
-		<div id="fragOptions">{TAB_OPTIONS}</div>
-	</div>';
-	
-	$strFragEditorContent = '
-		<form action="'.$_SERVER["REQUEST_URI"].'" method="POST" id="form_editor" onSubmit="return actionClickOnSaveHtml(this,\''.$pfile->getUrl().'\',\''.urljsencode($_GET['file']).'\');" style="text-align:right">
-			<div class="fckEditor">
-				'.$oFCKeditor->CreateHtml().'
-			<div class="panelHistory">
-				<h4>'._('History').'</h4>
-				{HISTORY_CONTENT}
-			</div><!-- end history -->
-			</div><!-- end fckblock -->
-			<!-- buttons -->
-			<div style="text-align:right;clear:both;">
-				<button class="ui-state-default ui-corner-all" type="button" onClick="MyCancel();">'._('cancel').'</button>
-				<button class="ui-state-default ui-corner-all" type="button" onClick="$(\'.openTabs:first\').trigger(\'click\');">'._('history').'</button>
-				<button class="ui-state-default ui-corner-all" type="submit" onClick="this.form.elements[\'view\'].value=true;">'._('save & see').'</button>
-				<button class="ui-state-default ui-corner-all" type="submit">'._('save').'</button>
-			</div>
-			<input type="hidden" value="false" name="view" />
-			<input type="hidden" name="todo" value="save" />
-		</form>
-	';
-	/**
-	 * GESTION DE L'HISTORIQUE
-	 */
-	$strHistoryContent='<ul class="historylist">{LIST_HISTORY}</ul>';
-	$strHtmlListHistory='';
-	$tabHistory = &$pfile->getHistoryList();
-	if(sizeof($tabHistory) == 0)
-		$strHistoryContent = _('History empty !');
-	else {
-		$i=0;
-		foreach($tabHistory as $elemHistory){
-			$strHtmlListHistory.='<li><a class="itemHistory '.(($i==0)?'selected':'').'" href="#" onclick="loadHistoryPage(\''.$elemHistory['PATH'].'\',this);return false;">'.$elemHistory['PRINTNAME'].'</a></li>';
-			$i++;
-		}				
-		$strHistoryContent = str_replace('{LIST_HISTORY}',$strHtmlListHistory,$strHistoryContent);
-	}
-	$strFragEditorContent = str_replace('{HISTORY_CONTENT}',$strHistoryContent,$strFragEditorContent);
+	echo '
+		</div>';
 
-	$strFragOptionsContent = $pfile->oPConfigFile->DisplayEditor();
-	echo str_replace(array('{TAB_EDITOR}','{TAB_OPTIONS}'),array($strFragEditorContent,$strFragOptionsContent),$strTabsTpl);
-
-} else{//if not html page
 	echo $pfile->DisplayEditor();
-}
-?>
-
-<?php
 }//end user connected
 include("admin_bottom.php");
 ?>
